6341dc014f2d2991def6f7f6ead8392884e867b4
rehrenreich <ryanse@gmail.com>
2023-10-15 17:04:26 -0700

    * Implemented alternate set of Firebase functions that use typed collection descriptors when creating messages.

--- quirks/timestamp_construction/functions/src/index.ts
+++ quirks/timestamp_construction/functions/src/index.ts
@@ -1,6 +1,7 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from "firebase-admin";
+import * as OPA from "../../base/src";
 import * as DataModel from "../../data_model/src";
 
 const app = admin.initializeApp();
@@ -24,6 +25,27 @@ export const saveMessageWithDateFromDataModel = onRequest(httpsOptions,
   }
 );
 
+export const saveMessageWithDateFromDataModelTyped = onRequest(httpsOptions,
+  (request, response) => {
+    logger.info("saveMessageWithDateFromDataModelTyped(...) ENTERED", {structuredData: true});
+    const message = (request.query.message) ? (request.query.message as string) : "Test Message";
+
+    const db = admin.firestore(app);
+    // NOTE: The unsafe cast on the next line is used to simplify the code to 1 line for this example ONLY
+    const dataStorageState = (({db} as unknown) as OPA.IDataStorageState);
+
+    const colRef = DataModel.MessageWithDateCollectionDescriptor.getTypedCollection(dataStorageState);
+    const docRef = colRef.doc();
+    const docId = docRef.id;
+
+    const messageDoc = DataModel.createMessageWithDate(docId, message);
+    docRef.set(messageDoc);
+
+    response.send(messageDoc);
+    logger.info("saveMessageWithDateFromDataModelTyped(...) COMPLETED", {structuredData: true});
+  }
+);
+
 export const saveMessageWithDateFromFunctions = onRequest(httpsOptions,
   (request, response) => {
     logger.info("saveMessageWithDateFromFunctions(...) ENTERED", {structuredData: true});
@@ -40,6 +62,28 @@ export const saveMessageWithDateFromFunctions = onRequest(httpsOptions,
 
     response.send(messageDoc);
     logger.info("saveMessageWithDateFromFunctions(...) COMPLETED", {structuredData: true});
+  }
+);
+
+export const saveMessageWithDateFromFunctionsTyped = onRequest(httpsOptions,
+  (request, response) => {
+    logger.info("saveMessageWithDateFromFunctionsTyped(...) ENTERED", {structuredData: true});
+    const message = (request.query.message) ? (request.query.message as string) : "Test Message";
+    const now = new Date();
+
+    const db = admin.firestore(app);
+    // NOTE: The unsafe cast on the next line is used to simplify the code to 1 line for this example ONLY
+    const dataStorageState = (({db} as unknown) as OPA.IDataStorageState);
+
+    const colRef = DataModel.MessageWithDateCollectionDescriptor.getTypedCollection(dataStorageState);
+    const docRef = colRef.doc();
+    const docId = docRef.id;
+
+    const messageDoc = DataModel.createMessageWithDate(docId, message, now);
+    docRef.set(messageDoc);
+
+    response.send(messageDoc);
+    logger.info("saveMessageWithDateFromFunctionsTyped(...) COMPLETED", {structuredData: true});
   }
 );
 
@@ -61,6 +105,27 @@ export const saveMessageWithTimestampFromDataModel = onRequest(httpsOptions,
   }
 );
 
+export const saveMessageWithTimestampFromDataModelTyped = onRequest(httpsOptions,
+  (request, response) => {
+    logger.info("saveMessageWithTimestampFromDataModelTyped(...) ENTERED", {structuredData: true});
+    const message = (request.query.message) ? (request.query.message as string) : "Test Message";
+
+    const db = admin.firestore(app);
+    // NOTE: The unsafe cast on the next line is used to simplify the code to 1 line for this example ONLY
+    const dataStorageState = (({db} as unknown) as OPA.IDataStorageState);
+
+    const colRef = DataModel.MessageWithTimestampCollectionDescriptor.getTypedCollection(dataStorageState);
+    const docRef = colRef.doc();
+    const docId = docRef.id;
+
+    const messageDoc = DataModel.createMessageWithTimestamp(docId, message);
+    docRef.set(messageDoc);
+
+    response.send(messageDoc);
+    logger.info("saveMessageWithTimestampFromDataModelTyped(...) COMPLETED", {structuredData: true});
+  }
+);
+
 export const saveMessageWithTimestampFromFunctions = onRequest(httpsOptions,
   (request, response) => {
     logger.info("saveMessageWithTimestampFromFunctions(...) ENTERED", {structuredData: true});
@@ -77,5 +142,27 @@ export const saveMessageWithTimestampFromFunctions = onRequest(httpsOptions,
 
     response.send(messageDoc);
     logger.info("saveMessageWithTimestampFromFunctions(...) COMPLETED", {structuredData: true});
+  }
+);
+
+export const saveMessageWithTimestampFromFunctionsTyped = onRequest(httpsOptions,
+  (request, response) => {
+    logger.info("saveMessageWithTimestampFromFunctionsTyped(...) ENTERED", {structuredData: true});
+    const message = (request.query.message) ? (request.query.message as string) : "Test Message";
+    const now = admin.firestore.Timestamp.now();
+
+    const db = admin.firestore(app);
+    // NOTE: The unsafe cast on the next line is used to simplify the code to 1 line for this example ONLY
+    const dataStorageState = (({db} as unknown) as OPA.IDataStorageState);
+
+    const colRef = DataModel.MessageWithTimestampCollectionDescriptor.getTypedCollection(dataStorageState);
+    const docRef = colRef.doc();
+    const docId = docRef.id;
+
+    const messageDoc = DataModel.createMessageWithTimestamp(docId, message, now);
+    docRef.set(messageDoc);
+
+    response.send(messageDoc);
+    logger.info("saveMessageWithTimestampFromFunctionsTyped(...) COMPLETED", {structuredData: true});
   }
 );
